93ab58089f915d80923d6962b62598c6ff869550
Scott Gibbs <55638818+kangabru@users.noreply.github.com>
2023-06-02 08:28:10 +0200

    Added demo react project for testing the library via a UI

---
+++ react-demo-ts/src/App.tsx
@@ -0,0 +1,41 @@
+import mod from "react-event-modifiers"
+
+export default function TestPage() {
+  return (
+    <main
+      className="w-full max-w-screen-sm mx-auto p-10 m-10 bg-gray-100 rounded-2xl space-y-4"
+      onClick={() => console.log("You've clicked on <main>")}
+    >
+      <p className="text-xl">Open dev tools and click on elements below</p>
+
+      <div onClick={logElementName}>DIV: Normal</div>
+
+      <mod.div onClick-stopPropagation={logElementName}>
+        DIV: StopPropagation
+      </mod.div>
+
+      <mod.div onClick-stopPropagation>DIV: StopPropagation with flag</mod.div>
+
+      <form>
+        <button type="submit">FORM: Normal</button>
+      </form>
+
+      <mod.form onSubmit-preventDefault={logElementName}>
+        <button type="submit">FORM: PreventDefault</button>
+      </mod.form>
+
+      <mod.form
+        onClick-stopPropagation
+        onSubmit-preventDefault={logElementName}
+      >
+        <button type="submit">
+          FORM: PreventDefault and Click StopPropagation
+        </button>
+      </mod.form>
+    </main>
+  )
+}
+
+function logElementName(e: any) {
+  console.log(`You've clicked on [${e.currentTarget.innerText}]`)
+}
